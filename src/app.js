1b8f2ed1a92c97b472af086d95b3f83abbd15673
bhanujggandhi <gandhibhanuj@gmail.com>
2020-05-12 02:56:17 +0530

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
+
+ReactDOM.render(<p>Boilerplate</p>, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    path: path.join(__dirname, "public/"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        loader: "babel-loader",
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  devtool: "cheap-module-eval-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "public/"),
+    historyApiFallback: true,
+  },
+};
+
+console.log(path.join(__dirname, "public/"));
