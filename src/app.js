02960b6b99ba572bbd40f148be47b08ff2d9ca0d
bhanujggandhi <gandhibhanuj@gmail.com>
2020-05-12 03:03:42 +0530

    Routes added

--- src/app.js
+++ src/app.js
@@ -4,4 +4,23 @@ import { BrowserRouter, Route } from "react-router-dom";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-ReactDOM.render(<p>Boilerplate</p>, document.getElementById("root"));
+const ExpenseDashboardPage = () => <div>This is my dashboard page</div>;
+
+const AddExpensePage = () => <div>This is my add expense page</div>;
+
+const EditExpensePage = () => <div>This is my Edit expense page</div>;
+
+const HelpPage = () => <div>How can I help?</div>;
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("root"));
