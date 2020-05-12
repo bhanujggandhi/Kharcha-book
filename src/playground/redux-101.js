12d603990eeb79ac121f0749b5188e658878ce3e
bhanujggandhi <gandhibhanuj@gmail.com>
2020-05-12 23:21:28 +0530

    Redux properties

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -3,13 +3,17 @@ import { createStore } from "redux";
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case "INCREMENT":
+      const incrementBy =
+        typeof action.incrementBy === "number" ? action.incrementBy : 1;
       return {
-        count: state.count + 1,
+        count: state.count + incrementBy,
       };
 
     case "DECREMENT":
+      const decrementBy =
+        typeof action.decrementBy === "number" ? action.decrementBy : 1;
       return {
-        count: state.count - 1,
+        count: state.count - decrementBy,
       };
 
     case "RESET":
@@ -19,8 +23,15 @@ const store = createStore((state = { count: 0 }, action) => {
   }
   return state;
 });
-console.log(store.getState());
 
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch({
+  type: "INCREMENT",
+  incrementBy: 5,
+});
 store.dispatch({
   type: "INCREMENT",
 });
@@ -29,6 +40,7 @@ store.dispatch({
 });
 store.dispatch({
   type: "DECREMENT",
+  decrementBy: 10,
 });
 store.dispatch({
   type: "DECREMENT",
@@ -39,5 +51,3 @@ store.dispatch({
 store.dispatch({
   type: "INCREMENT",
 });
-
-console.log(store.getState());
