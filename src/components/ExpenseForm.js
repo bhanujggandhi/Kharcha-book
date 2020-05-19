480a0e222fca4940a2f901fa4281982956f9e3b5
bhanujggandhi <gandhibhanuj@gmail.com>
2020-05-19 18:27:38 +0530

    Edit form||Redux devtools

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -3,18 +3,18 @@ import moment from "moment";
 import { SingleDatePicker } from "react-dates";
 import "react-dates/lib/css/_datepicker.css";
 
-const now = moment();
-console.log(now.format("MMM Do, YYYY"));
-
 class ExpenseForm extends Component {
-  state = {
-    description: "",
-    note: "",
-    amount: "",
-    createdAt: moment(),
-    calenderFocus: false,
-    error: "",
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      description: props.expense ? props.expense.description : "",
+      note: props.expense ? props.expense.note : "",
+      amount: props.expense ? props.expense.amount.toString() : "",
+      createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
+      calenderFocus: false,
+      error: "",
+    };
+  }
   onDescriptionChange = (event) => {
     const description = event.target.value;
     this.setState(() => ({ description }));
