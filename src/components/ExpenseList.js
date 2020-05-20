ec0b15c492a26c03245488709d7ab023317cb501
bhanujggandhi <gandhibhanuj@gmail.com>
2020-05-21 04:44:50 +0530

    List component tested

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -3,12 +3,15 @@ import { connect } from "react-redux";
 import ExpenseListItem from "./ExpenseListItem";
 import getVisibleExpenses from "../selectors/expenses";
 
-const ExpenseList = (props) => (
+export const ExpenseList = (props) => (
   <div>
-    <h1>Expense List</h1>
-    {props.expenses.map((expense) => {
-      return <ExpenseListItem {...expense} key={expense.id} />;
-    })}
+    {props.expenses.length === 0 ? (
+      <p>No expenses</p>
+    ) : (
+      props.expenses.map((expense) => {
+        return <ExpenseListItem {...expense} key={expense.id} />;
+      })
+    )}
   </div>
 );
 
