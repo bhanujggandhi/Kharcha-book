78d7d7cfea3a52a0d71dfaea6070e205e73b2b61
bhanujggandhi <gandhibhanuj@gmail.com>
2020-05-21 04:14:50 +0530

    enzyme test added for components

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,9 +1,13 @@
 import React from "react";
-import ReactShallowRenderer from "react-test-renderer/shallow";
+import { shallow } from "enzyme";
 import Header from "../../components/Header";
 
 test("should render Header correctly", () => {
-  const renderer = new ReactShallowRenderer();
-  renderer.render(<Header />);
-  expect(renderer.getRenderOutput()).toMatchSnapshot();
+  const wrapper = shallow(<Header />);
+  expect(wrapper).toMatchSnapshot();
+
+  // expect(wrapper.find("h1").text()).toBe("Expense Manager");
+  // const renderer = new ReactShallowRenderer();
+  // renderer.render(<Header />);
+  // expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
