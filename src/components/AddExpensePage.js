913b2721984dcf9eae08bbb1d3fec52049af0809
bhanujggandhi <gandhibhanuj@gmail.com>
2020-05-22 18:19:03 +0530

    Made Add component testable

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -8,11 +8,16 @@ const AddExpensePage = (props) => (
     <h1>Add Expense</h1>
     <ExpenseForm
       onSubmit={(expense) => {
-        props.dispatch(addExpense(expense));
+        // props.dispatch(addExpense(expense));
+        props.onSubmit(expense);
         props.history.push("/");
       }}
     />
   </div>
 );
 
-export default connect()(AddExpensePage);
+const mapDispatchToProps = (dispatch) => ({
+  onSubmit: (expense) => dispatch(addExpense(expense)),
+});
+
+export default connect(undefined, mapDispatchToProps)(AddExpensePage);
