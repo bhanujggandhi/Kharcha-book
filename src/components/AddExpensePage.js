3ae14e5133f2e88814e5b04649fc1c3aa4253bd7
bhanujggandhi <gandhibhanuj@gmail.com>
2020-05-22 18:30:46 +0530

    Class based component Add

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -3,18 +3,21 @@ import { connect } from "react-redux";
 import ExpenseForm from "./ExpenseForm";
 import { addExpense } from "../actions/expenses";
 
-const AddExpensePage = (props) => (
-  <div>
-    <h1>Add Expense</h1>
-    <ExpenseForm
-      onSubmit={(expense) => {
-        // props.dispatch(addExpense(expense));
-        props.onSubmit(expense);
-        props.history.push("/");
-      }}
-    />
-  </div>
-);
+export class  extends Component {
+  onSubmit = (expense) => {
+    // props.dispatch(addExpense(expense));
+    props.onSubmit(expense);
+    props.history.push("/");
+  }
+  render() { 
+    return ( 
+    <div>
+      <h1>Add Expense</h1>
+      <ExpenseForm onSubmit={this.onSubmit}/>
+    </div> 
+    );
+  }
+}
 
 const mapDispatchToProps = (dispatch) => ({
   onSubmit: (expense) => dispatch(addExpense(expense)),
