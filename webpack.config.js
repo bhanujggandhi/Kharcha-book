e6f0cb16c0fb748f9f9eae317499e530e31d33d9
bhanujggandhi <gandhibhanuj@gmail.com>
2020-05-23 06:10:52 +0530

    prod setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,38 @@
 const path = require("path");
 
-module.exports = {
-  entry: "./src/app.js",
-  output: {
-    path: path.join(__dirname, "public/"),
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        loader: "babel-loader",
-        test: /\.js$/,
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.s?css$/,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
-      },
-    ],
-  },
-  devtool: "cheap-module-eval-source-map",
-  devServer: {
-    contentBase: path.join(__dirname, "public/"),
-    historyApiFallback: true,
-  },
-};
+module.exports = (env) => {
+  const isProduction = env === "production";
 
-console.log(path.join(__dirname, "public/"));
+  return {
+    entry: "./src/app.js",
+    output: {
+      path: path.join(__dirname, "public/"),
+      filename: "bundle.js",
+    },
+    module: {
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s?css$/,
+          use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+          ],
+        },
+      ],
+    },
+    devtool: isProduction ? "source-map" : "cheap-module-eval-source-map",
+    devServer: {
+      contentBase: path.join(__dirname, "public/"),
+      historyApiFallback: true,
+    },
+  };
+};
