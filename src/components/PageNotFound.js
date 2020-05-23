1d6c6671cdd3ea24b94586a44bfbb080c7ebdff9
bhanujggandhi <gandhibhanuj@gmail.com>
2020-05-23 07:00:42 +0530

    prod

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,5 +1,9 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
-const PageNotFound = () => <div>404! Not found</div>;
-
+const PageNotFound = () => (
+  <div>
+    404-<NavLink to="/">Go Home</NavLink>
+  </div>
+);
 export default PageNotFound;
