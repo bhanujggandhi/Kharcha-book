cb98ec74e320aca69035f2df6007ffd6dce61a19
bhanujggandhi <gandhibhanuj@gmail.com>
2020-05-23 05:49:18 +0530

    Removed dummy fata

--- src/app.js
+++ src/app.js
@@ -3,35 +3,11 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
-import { addExpense } from "./actions/expenses";
-import { setTextFilter } from "./actions/filters";
-import getVisibleExpenses from "./selectors/expenses";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 import "react-dates/lib/css/_datepicker.css";
 
 const store = configureStore();
-
-store.dispatch(
-  addExpense({
-    description: "WATER BILL",
-    note: "For January",
-    amount: 200,
-    createdAt: -24000,
-  })
-);
-store.dispatch(
-  addExpense({
-    description: "GAS BILL",
-    note: "FOR 3 MONTHS",
-    amount: 7,
-    createdAt: -1000,
-  })
-);
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
