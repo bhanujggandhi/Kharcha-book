cb4b743054d74c5b9a425c70afb2d87ddc3790c8
bhanujggandhi <gandhibhanuj@gmail.com>
2020-05-28 13:59:49 +0530

    bug fb fixed

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || "development";
 if (process.env.NODE_ENV === "test") {
   require("dotenv").config({ path: ".env.test" });
 } else if (process.env.NODE_ENV === "development") {
-  require("dotenv").config({ path: ".env.test" });
+  require("dotenv").config({ path: ".env.development" });
 }
 
 module.exports = (env) => {
