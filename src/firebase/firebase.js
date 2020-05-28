adbe03b442b0cfda42226b09d1d1702af72b923a
bhanujggandhi <gandhibhanuj@gmail.com>
2020-05-28 13:33:19 +0530

    setup test database env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,14 @@
 import * as firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAQObgSkVuVFs1n7q9h5Z17apzfymLxmII",
-  authDomain: "kharcha-book.firebaseapp.com",
-  databaseURL: "https://kharcha-book.firebaseio.com",
-  projectId: "kharcha-book",
-  storageBucket: "kharcha-book.appspot.com",
-  messagingSenderId: "168480619982",
-  appId: "1:168480619982:web:8139c22d3d0a2d1d213f9f",
-  measurementId: "G-YJEDQ7DNX3",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
