09267a667d8c77e975fbfd53086579bf320ff3f8
bhanujggandhi <gandhibhanuj@gmail.com>
2020-05-29 19:55:16 +0530

    loginpage styled

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -4,8 +4,12 @@ import { startLogin } from "../actions/auth";
 
 export const LoginPage = ({ startLogin }) => {
   return (
-    <div>
-      <button onClick={startLogin}>Login</button>
+    <div className="box-layout">
+      <div className="box-layout__box">
+        <h1 className=".box-layout__title">Kharcha Book</h1>
+        <p>It's time to get your expenses under control.</p>
+        <button onClick={startLogin}>Login</button>
+      </div>
     </div>
   );
 };
